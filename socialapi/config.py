55aac3a37f7d0396219a2a9f6f268fa2bee22bbf
mano.beeslaar <mano.beeslaar@gmail.com>
2023-12-27 19:10:02 +0200

    created env & database structure with sqlalchemy

---
+++ socialapi/config.py
@@ -0,0 +1,39 @@
+from functools import lru_cache
+from typing import Optional
+
+from pydantic import BaseSettings, SettingsConfigDict
+
+
+class BaseConfig(BaseSettings):
+    ENV_STATE: Optional[str] = None
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+        
+class GlobalConfig(BaseConfig):
+    DATABASE_URL: Optional[str] = None
+    DB_FORCE_ROLL_BACK: bool = False
+    
+class DevConfig(GlobalConfig):
+    model_config: SettingsConfigDict = {"env_prefix": "DEV_"}
+
+class TestConfig(GlobalConfig):
+    DATABASE_URL: str = "sqlite:///./test.db"
+    DB_FORCE_ROLL_BACK: bool = True
+    model_config: SettingsConfigDict = {"env_prefix": "TEST_"}
+
+class ProdConfig(GlobalConfig):
+    model_config: SettingsConfigDict = {"env_prefix": "PROD_"}
+
+@lru_cache()
+def get_config(env_state: str):
+    configs = {
+        "dev": DevConfig(),
+        "test": TestConfig(),
+        "prod": ProdConfig()
+    }
+    return configs[env_state]()
+
+config = get_config(BaseConfig().ENV_STATE)
+
+    
